7efe9c93dbb3fdd77041124ada766de6d6edee8f
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-03-03 19:53:53 -0500

    heroku music

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(session({
     secret: secret,
